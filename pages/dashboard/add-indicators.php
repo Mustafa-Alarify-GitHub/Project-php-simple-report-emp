961067eda4670839af0f4bd8d8dc75eecbcf6d17
Mustafa-Alarify <m2024u775516215@gmail.com>
2025-02-15 21:28:26 +0300

    Update sidebar navigation in add-indicators page to include additional links

--- pages/dashboard/add-indicators.php
+++ pages/dashboard/add-indicators.php
@@ -15,15 +15,18 @@
     <div class="sidebar">
         <h2>لوحة التحكم</h2>
         <ul>
-            <li><a href="../"> الرئيسية</a></li>
-            <li><a href="/simple-report-emp/pages/dashboard/add-criteria.php"> إضافة معيار جديد</a></li>
-            <li><a href="/simple-report-emp/pages/dashboard/add-indicators.php"> أداره الموشرات</a></li>
+        <li><a href="../"> الرئيسية</a></li>
+            <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-criteria.php"> إضافة معيار جديد</a></li>
+            <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-indicators.php"> أداره الموشرات</a></li>
+            <li><a href="/Project-php-simple-report-emp/pages/dashboard/show-emp.php"> قائمة الموظفين</a></li>
+            <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-emp.php"> أضافه الموظفين</a></li>
+            <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-jop.php"> أضافه الوظيفة</a></li>
+            <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-category.php"> أضافه قسم </a></li>
 
         </ul>
     </div>
 
 
-    <div class="container">
         <div class="container">
             <h2>إدارة المؤشرات</h2>
 
