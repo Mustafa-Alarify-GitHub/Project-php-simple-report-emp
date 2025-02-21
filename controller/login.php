4bd912332ea0dbc8bb1e5e802e63c0aa4373ac94
Mustafa-Alarify <m2024u775516215@gmail.com>
2025-02-21 20:14:14 +0300

    Enhance login functionality to set cookies and session variables for user details upon successful authentication

--- controller/login.php
+++ controller/login.php
@@ -8,15 +8,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($username == "admin" && $password == "admin") {
         header("Location: ../pages/dashboard/add-category.php");
+        setcookie("name", "ِAdmin", time() + (7 * 24 * 60 * 60), "/");
+        setcookie("isAdmin", true, time() + (7 * 24 * 60 * 60), "/");
+        $_SESSION["name"] = "ِAdmin";
+        $_SESSION["isAdmin"] = true;
     } else
-        $stmt = $con->prepare("SELECT * FROM employees WHERE name = ? and password = ? ");
+    $stmt = $con->prepare("SELECT * FROM employees WHERE name = ? and password = ? ");
     $stmt->execute([$username, $password]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
     $count  = $stmt->rowCount();
 
     if ($count > 0) {
 
-        setcookie("name", $username, time() + (7 * 24 * 60 * 60), "/");
-        $_SESSION["username"] = $username;
+        setcookie("id", $user["id"], time() + (7 * 24 * 60 * 60), "/");
+        setcookie("name", $user["name"], time() + (7 * 24 * 60 * 60), "/");
+        setcookie("job_id", $user["job_id"], time() + (7 * 24 * 60 * 60), "/");
+        setcookie("category_id", $user["category_id"], time() + (7 * 24 * 60 * 60), "/");
+    
+        $_SESSION["id"] = $user["id"];
+        $_SESSION["name"] = $username;
+        $_SESSION["job_id"] = $user["job_id"];
+        $_SESSION["category_id"] = $user["category_id"];
 
         header("Location: ../pages/index.php");
         exit();
