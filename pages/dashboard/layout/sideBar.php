8a9a26fc2457f7601f7bfdc17f219e66f545a787
Mustafa-Alarify <m2024u775516215@gmail.com>
2025-02-22 10:05:46 +0300

    Add session management and logout link to sidebar

--- pages/dashboard/layout/sideBar.php
+++ pages/dashboard/layout/sideBar.php
@@ -1,3 +1,13 @@
+
+<?php
+session_start();
+if (!isset($_SESSION["name"]) && !isset($_COOKIE["name"])) {
+    header("Location: ../../pages/login.php");
+    exit();
+}
+
+$username = $_SESSION["name"] ?? $_COOKIE["name"];
+?>
 <!DOCTYPE html>
 <html lang="ar">
 
@@ -16,12 +26,13 @@
     <div class="sidebar">
         <h2>لوحة التحكم</h2>
         <ul>
-            <li><a href="../"> الرئيسية</a></li>
             <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-criteria.php"> إضافة معيار جديد</a></li>
             <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-indicators.php"> أداره الموشرات</a></li>
             <li><a href="/Project-php-simple-report-emp/pages/dashboard/show-emp.php"> قائمة الموظفين</a></li>
             <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-emp.php"> أضافه الموظفين</a></li>
             <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-jop.php"> أضافه الوظيفة</a></li>
             <li><a href="/Project-php-simple-report-emp/pages/dashboard/add-category.php"> أضافه قسم </a></li>
+            <a style="color: aliceblue;" href="../../controller/logout.php" class="link link-logout">تسجيل الخروج</a>
+       
         </ul>
     </div>
