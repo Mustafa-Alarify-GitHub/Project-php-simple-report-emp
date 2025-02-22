0614ab6213e7b91dcc4228d1a7fcee35b963a2e4
Mustafa-Alarify <m2024u775516215@gmail.com>
2025-02-22 10:05:27 +0300

    Clear session cookies on logout to enhance security

--- controller/logout.php
+++ controller/logout.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
 setcookie("name", "", time() - 3600, "/");
+setcookie("id", "", time() - 3600, "/");
+setcookie("job_id", "", time() - 3600, "/");
+setcookie("category_id", "", time() - 3600, "/");
 session_unset();
 session_destroy();
 header("Location: ../pages/login.php");
 exit();
-?>
